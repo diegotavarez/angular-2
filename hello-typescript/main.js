98434199b5340f5cdb8fe54d540dd0210d4bafe6
diegotavarez <tavares.diego@outlook.com>
2017-03-27 23:31:24 -0300

    first-commit

---
+++ hello-typescript/main.js
@@ -0,0 +1,13 @@
+//JAVASCRIPT PURO
+var minhaVar = 'minha variavel';
+function minhaFunc(x, y) {
+    return x + y;
+}
+var numeros = [1, 2, 3];
+numeros.map(function (valor) {
+    return valor * 2;
+});
+//ECMA SCRIPT 2015 ou ES6
+var num = 2;
+var PI = 3.14;
+numeros.map(function (valor) { return valor * 2; }); //ES6 arrow function
---
+++ hello-typescript/main.ts
@@ -0,0 +1,17 @@
+//JAVASCRIPT PURO
+var minhaVar = 'minha variavel';
+
+function minhaFunc(x,y){
+  return x + y;
+}
+
+var numeros = [1,2,3];
+numeros.map(function(valor){
+  return valor*2;
+});
+
+//ECMA SCRIPT 2015 ou ES6
+let num =2;
+const PI = 3.14;
+
+numeros.map(valor => valor * 2); //ES6 arrow function
