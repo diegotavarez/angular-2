961f80402692635bcfe129bc99fd4abdeb095854
diegotavarez <tavares.diego@outlook.com>
2017-03-29 15:19:21 -0300

    Event binding

--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -3,13 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-data-binding',
   templateUrl: './data-binding.component.html',
-  styleUrls: ['./data-binding.component.css']
+  styleUrls: ['./data-binding.component.css'],
 })
 export class DataBindingComponent implements OnInit {
 
   url: string = 'http:/loiane.com';
   cursoAngular: boolean = true;
   urlImagem:string = 'http://lorempixel.com/400/200/nature/';
+
+  conteudoAtual:string = '';
+  conteudoSalvo:string = '';
+
+  isMouseOver: boolean = false;
 
   getValor(){
     return 1;
@@ -22,6 +27,23 @@ export class DataBindingComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onClick(){
+    alert('Botão clicado!');
+  }
+
+  onKeyUp(event:any){
+    console.log(event);
+    this.conteudoAtual = (<HTMLInputElement> event.target).value;
+  }
+
+  onSave(valor:string){
+    this.conteudoSalvo = valor;
+  }
+
+  onMouseSpan(){
+    this.isMouseOver = !this.isMouseOver;
   }
 
 }
