75460cf901a6866cce103c3de6300c21d7da2716
diegotavarez <tavares.diego@outlook.com>
2017-05-06 12:23:45 -0300

    criando um modulo de funcionalidade

--- rotas/src/app/app.module.ts
+++ rotas/src/app/app.module.ts
@@ -8,30 +8,28 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
-import { CursosComponent } from './cursos/cursos.component';
-import { routing } from './app.routing';
-import { CursoDetalheComponent } from './cursos/curso-detalhe/curso-detalhe.component';
-import { CursosService } from './cursos/cursos.service';
-import { CursoNaoEncontradoComponent } from './cursos/curso-nao-encontrado/curso-nao-encontrado.component';
+// import { routing } from './app.routing';
+
+import { CursosModule } from './cursos/cursos.module';
+
 import { AppRoutingModule } from './app.routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    LoginComponent,
-    CursosComponent,
-    CursoDetalheComponent,
-    CursoNaoEncontradoComponent
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
+    CursosModule,
     AppRoutingModule
     // routing
   ],
-  providers: [CursosService],
+  providers: [
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- rotas/src/app/app.routing.ts
+++ rotas/src/app/app.routing.ts
@@ -1,18 +1,18 @@
-import { Routes, RouterModule } from '@angular/router';
-import { ModuleWithProviders } from '@angular/core';
-
-import { HomeComponent } from './home/home.component';
-import { CursosComponent } from './cursos/cursos.component';
-import { LoginComponent } from './login/login.component';
-import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
-import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-encontrado.component';
-
-const APP_ROUTES: Routes = [
-  { path: 'cursos', component: CursosComponent},
-  { path: 'curso/:id', component: CursoDetalheComponent},
-  { path: 'login', component: LoginComponent },
-  { path: '', component: HomeComponent },
-  { path: 'naoEncontrado', component: CursoNaoEncontradoComponent}
-];
-
-export const routing: ModuleWithProviders = RouterModule.forRoot(APP_ROUTES);
+// import { Routes, RouterModule } from '@angular/router';
+// import { ModuleWithProviders } from '@angular/core';
+//
+// import { HomeComponent } from './home/home.component';
+// import { CursosComponent } from './cursos/cursos.component';
+// import { LoginComponent } from './login/login.component';
+// import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
+// import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-encontrado.component';
+//
+// const APP_ROUTES: Routes = [
+//   { path: 'cursos', component: CursosComponent},
+//   { path: 'curso/:id', component: CursoDetalheComponent},
+//   { path: 'login', component: LoginComponent },
+//   { path: '', component: HomeComponent },
+//   { path: 'naoEncontrado', component: CursoNaoEncontradoComponent}
+// ];
+//
+// export const routing: ModuleWithProviders = RouterModule.forRoot(APP_ROUTES);
