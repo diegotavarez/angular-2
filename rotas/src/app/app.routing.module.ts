cd7222f6697f9d954329b03501bc43c991ebed65
diegotavarez <tavares.diego@outlook.com>
2017-05-06 12:33:50 -0300

    Criando um modulo de rotas de funcionalidade

--- rotas/src/app/app.routing.module.ts
+++ rotas/src/app/app.routing.module.ts
@@ -3,20 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { ModuleWithProviders } from '@angular/core';
 
 import { HomeComponent } from './home/home.component';
-import { CursosComponent } from './cursos/cursos.component';
 import { LoginComponent } from './login/login.component';
-import { CursoDetalheComponent } from './cursos/curso-detalhe/curso-detalhe.component';
-import { CursoNaoEncontradoComponent } from './cursos/curso-nao-encontrado/curso-nao-encontrado.component';
+
 
 const appRoutes: Routes = [
-  { path: 'cursos', component: CursosComponent},
-  { path: 'curso/:id', component: CursoDetalheComponent},
   { path: 'login', component: LoginComponent },
-  { path: '', component: HomeComponent },
-  { path: 'naoEncontrado', component: CursoNaoEncontradoComponent}
+  { path: '', component: HomeComponent }
 ];
-
-// export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
 
 @NgModule({
   imports: [RouterModule.forRoot(appRoutes)],
