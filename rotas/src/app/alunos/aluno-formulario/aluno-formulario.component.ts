42b32ea1e54ed3432d71456b5980f68ef0f1505f
diegotavarez <tavares.diego@outlook.com>
2017-05-07 12:03:25 -0300

    desenvolvendo as telas das rotas filhas

--- rotas/src/app/alunos/aluno-formulario/aluno-formulario.component.ts
+++ rotas/src/app/alunos/aluno-formulario/aluno-formulario.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs/Rx';
+
+import{ AlunosService } from '../alunos.service';
 
 @Component({
   selector: 'app-aluno-formulario',
@@ -6,10 +10,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./aluno-formulario.component.css']
 })
 export class AlunoFormularioComponent implements OnInit {
+  aluno: any = {};
+  inscricao: Subscription;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private alunosService: AlunosService
+  ) { }
 
   ngOnInit() {
+    this.inscricao = this.route.params.subscribe(
+      (params:any) => {
+        let id = params['id'];
+        this.aluno = this.alunosService.getAluno(id);
+
+        if(this.aluno === null){
+          this.aluno = {};
+        }
+      }
+    );
+  }
+
+
+  ngOnDestroy(){
+    this.inscricao.unsubscribe();
   }
 
 }
--- rotas/src/app/alunos/alunos.module.ts
+++ rotas/src/app/alunos/alunos.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { AlunosComponent } from './alunos.component';
 import { AlunoFormularioComponent } from './aluno-formulario/aluno-formulario.component';
 import { AlunosRoutingModule } from './alunos.routing.module';
 import { AlunoDetalheComponent } from './aluno-detalhe/aluno-detalhe.component';
+import { AlunosService } from './alunos.service';
 
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
     AlunosRoutingModule
   ],
   declarations: [
@@ -17,6 +20,6 @@ import { AlunoDetalheComponent } from './aluno-detalhe/aluno-detalhe.component';
     AlunoDetalheComponent
 
   ],
-  providers: []
+  providers: [AlunosService]
 })
 export class AlunosModule { }
