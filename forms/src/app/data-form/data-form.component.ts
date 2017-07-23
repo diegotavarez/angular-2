f1ba1da7929adfede8de3ad234a541a3ecbe7d68
diegotavarez <tavares.diego@outlook.com>
2017-07-23 16:16:43 -0300

    forms reativos: fazendo a busca pelo cep

--- forms/src/app/data-form/data-form.component.ts
+++ forms/src/app/data-form/data-form.component.ts
@@ -71,4 +71,62 @@ export class DataFormComponent implements OnInit {
     }
   }
 
+  consultaCEP(){
+    let cep = this.formulario.get('endereco.cep').value;
+    cep = cep.replace(/\D/g, '');
+
+    if (cep != ''){
+      var validacep = /^[0-9]{8}$/;
+
+      //Valida o formato do CEP.
+      if(validacep.test(cep)) {
+          this.resetaDadosForm();
+        
+          this.http.get(`//viacep.com.br/ws/${cep}/json`)
+          .map(dados => dados.json())
+          .subscribe((dados) => this.populaDadosForm(dados));
+      }
+    }
+  }
+
+    populaDadosForm(dados){
+    // formulario.setValue({
+    //   nome: formulario.value.nome,
+    //   email: formulario.value.nome,
+    //   endereco: {
+    //     cep: dados.cep,
+    //     numero: '',
+    //     complemento: dados.complemento,
+    //     rua: dados.logradouro,
+    //     bairro: dados.bairro,
+    //     cidade: dados.localidade,
+    //     estado: dados.uf  
+    //   }
+    // });
+
+    this.formulario.patchValue({
+      endereco: {
+        cep: dados.cep,
+        complemento: dados.complemento,
+        rua: dados.logradouro,
+        bairro: dados.bairro,
+        cidade: dados.localidade,
+        estado: dados.uf  
+      }
+    });
+  }
+
+    resetaDadosForm(){
+      this.formulario.patchValue({
+        endereco: {
+          cep: null,
+          complemento: null,
+          rua: null,
+          bairro: null,
+          cidade: null,
+          estado: null 
+        }
+      });
+  }
+
 }
